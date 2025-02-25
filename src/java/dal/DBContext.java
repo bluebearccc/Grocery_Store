8ae560bb48d1ed780ea7191773d864fb4bab4732
anhhauan <anhtran080305@gmail.com>
2025-02-25 08:31:10 +0700

    them dbcontext

--- src/java/dal/DBContext.java
+++ src/java/dal/DBContext.java
@@ -4,10 +4,32 @@
  */
 package dal;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 /**
  *
  * @author admin
  */
 public class DBContext {
-    
+
+    protected Connection connection;
+    protected PreparedStatement ps;
+    protected ResultSet rs;
+
+    public Connection getConnection() {
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            String url = "jdbc:sqlserver://localhost:1433;databaseName=4USER_PRJ301";
+            String username = "sa";
+            String password = "123";
+            connection = DriverManager.getConnection(url, username, password);
+            return connection;
+        } catch (Exception e) {
+            System.out.println("Error " + e.getMessage() + " at DBContext");
+            return null;
+        }
+    }
 }
