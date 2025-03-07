3f4fa6c42fa4536682205ce638c07e1cb7cabb94
anhhauan <anhtran080305@gmail.com>
2025-03-07 22:30:32 +0700

    Sua DataBase

--- Db_Grocery.sql
+++ Db_Grocery.sql
@@ -1,6 +1,88 @@
+USE [master]
+GO
+/****** Object:  Database [DB_GroceryStore]    Script Date: 3/7/2025 10:27:42 PM ******/
+CREATE DATABASE [DB_GroceryStore]
+ CONTAINMENT = NONE
+ ON  PRIMARY 
+( NAME = N'DB_GroceryStore', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL16.HAIANH\MSSQL\DATA\DB_GroceryStore.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
+ LOG ON 
+( NAME = N'DB_GroceryStore_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL16.HAIANH\MSSQL\DATA\DB_GroceryStore_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
+ WITH CATALOG_COLLATION = DATABASE_DEFAULT, LEDGER = OFF
+GO
+ALTER DATABASE [DB_GroceryStore] SET COMPATIBILITY_LEVEL = 160
+GO
+IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
+begin
+EXEC [DB_GroceryStore].[dbo].[sp_fulltext_database] @action = 'enable'
+end
+GO
+ALTER DATABASE [DB_GroceryStore] SET ANSI_NULL_DEFAULT OFF 
+GO
+ALTER DATABASE [DB_GroceryStore] SET ANSI_NULLS OFF 
+GO
+ALTER DATABASE [DB_GroceryStore] SET ANSI_PADDING OFF 
+GO
+ALTER DATABASE [DB_GroceryStore] SET ANSI_WARNINGS OFF 
+GO
+ALTER DATABASE [DB_GroceryStore] SET ARITHABORT OFF 
+GO
+ALTER DATABASE [DB_GroceryStore] SET AUTO_CLOSE OFF 
+GO
+ALTER DATABASE [DB_GroceryStore] SET AUTO_SHRINK OFF 
+GO
+ALTER DATABASE [DB_GroceryStore] SET AUTO_UPDATE_STATISTICS ON 
+GO
+ALTER DATABASE [DB_GroceryStore] SET CURSOR_CLOSE_ON_COMMIT OFF 
+GO
+ALTER DATABASE [DB_GroceryStore] SET CURSOR_DEFAULT  GLOBAL 
+GO
+ALTER DATABASE [DB_GroceryStore] SET CONCAT_NULL_YIELDS_NULL OFF 
+GO
+ALTER DATABASE [DB_GroceryStore] SET NUMERIC_ROUNDABORT OFF 
+GO
+ALTER DATABASE [DB_GroceryStore] SET QUOTED_IDENTIFIER OFF 
+GO
+ALTER DATABASE [DB_GroceryStore] SET RECURSIVE_TRIGGERS OFF 
+GO
+ALTER DATABASE [DB_GroceryStore] SET  DISABLE_BROKER 
+GO
+ALTER DATABASE [DB_GroceryStore] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
+GO
+ALTER DATABASE [DB_GroceryStore] SET DATE_CORRELATION_OPTIMIZATION OFF 
+GO
+ALTER DATABASE [DB_GroceryStore] SET TRUSTWORTHY OFF 
+GO
+ALTER DATABASE [DB_GroceryStore] SET ALLOW_SNAPSHOT_ISOLATION OFF 
+GO
+ALTER DATABASE [DB_GroceryStore] SET PARAMETERIZATION SIMPLE 
+GO
+ALTER DATABASE [DB_GroceryStore] SET READ_COMMITTED_SNAPSHOT OFF 
+GO
+ALTER DATABASE [DB_GroceryStore] SET HONOR_BROKER_PRIORITY OFF 
+GO
+ALTER DATABASE [DB_GroceryStore] SET RECOVERY SIMPLE 
+GO
+ALTER DATABASE [DB_GroceryStore] SET  MULTI_USER 
+GO
+ALTER DATABASE [DB_GroceryStore] SET PAGE_VERIFY CHECKSUM  
+GO
+ALTER DATABASE [DB_GroceryStore] SET DB_CHAINING OFF 
+GO
+ALTER DATABASE [DB_GroceryStore] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
+GO
+ALTER DATABASE [DB_GroceryStore] SET TARGET_RECOVERY_TIME = 60 SECONDS 
+GO
+ALTER DATABASE [DB_GroceryStore] SET DELAYED_DURABILITY = DISABLED 
+GO
+ALTER DATABASE [DB_GroceryStore] SET ACCELERATED_DATABASE_RECOVERY = OFF  
+GO
+ALTER DATABASE [DB_GroceryStore] SET QUERY_STORE = ON
+GO
+ALTER DATABASE [DB_GroceryStore] SET QUERY_STORE (OPERATION_MODE = READ_WRITE, CLEANUP_POLICY = (STALE_QUERY_THRESHOLD_DAYS = 30), DATA_FLUSH_INTERVAL_SECONDS = 900, INTERVAL_LENGTH_MINUTES = 60, MAX_STORAGE_SIZE_MB = 1000, QUERY_CAPTURE_MODE = AUTO, SIZE_BASED_CLEANUP_MODE = AUTO, MAX_PLANS_PER_QUERY = 200, WAIT_STATS_CAPTURE_MODE = ON)
+GO
 USE [DB_GroceryStore]
 GO
-/****** Object:  Table [dbo].[Categories]    Script Date: 3/5/2025 10:17:47 AM ******/
+/****** Object:  Table [dbo].[Categories]    Script Date: 3/7/2025 10:27:43 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -15,7 +97,7 @@ CREATE TABLE [dbo].[Categories](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[OrderDetails]    Script Date: 3/5/2025 10:17:47 AM ******/
+/****** Object:  Table [dbo].[OrderDetails]    Script Date: 3/7/2025 10:27:43 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -27,7 +109,7 @@ CREATE TABLE [dbo].[OrderDetails](
 	[unit__price] [float] NOT NULL
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Orders]    Script Date: 3/5/2025 10:17:47 AM ******/
+/****** Object:  Table [dbo].[Orders]    Script Date: 3/7/2025 10:27:43 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -42,7 +124,7 @@ CREATE TABLE [dbo].[Orders](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Products]    Script Date: 3/5/2025 10:17:47 AM ******/
+/****** Object:  Table [dbo].[Products]    Script Date: 3/7/2025 10:27:43 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -66,7 +148,7 @@ CREATE TABLE [dbo].[Products](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Suppliers]    Script Date: 3/5/2025 10:17:47 AM ******/
+/****** Object:  Table [dbo].[Suppliers]    Script Date: 3/7/2025 10:27:43 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -82,7 +164,7 @@ CREATE TABLE [dbo].[Suppliers](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Users]    Script Date: 3/5/2025 10:17:47 AM ******/
+/****** Object:  Table [dbo].[Users]    Script Date: 3/7/2025 10:27:43 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -116,6 +198,14 @@ GO
 INSERT [dbo].[Categories] ([category__id], [category__name], [description]) VALUES (5, N'Vegetables', N'Edible plant vegetables')
 GO
 SET IDENTITY_INSERT [dbo].[Categories] OFF
+GO
+SET IDENTITY_INSERT [dbo].[Orders] ON 
+GO
+INSERT [dbo].[Orders] ([order__id], [user__id], [order__date]) VALUES (3, 5, CAST(N'2025-03-01' AS Date))
+GO
+INSERT [dbo].[Orders] ([order__id], [user__id], [order__date]) VALUES (4, 6, CAST(N'2025-03-01' AS Date))
+GO
+SET IDENTITY_INSERT [dbo].[Orders] OFF
 GO
 SET IDENTITY_INSERT [dbo].[Products] ON 
 GO
@@ -155,10 +245,6 @@ SET IDENTITY_INSERT [dbo].[Suppliers] OFF
 GO
 SET IDENTITY_INSERT [dbo].[Users] ON 
 GO
-INSERT [dbo].[Users] ([user__id], [username], [fullname], [password], [email], [address], [phone], [role], [balance]) VALUES (1, N'user01', N'Nguyễn Văn A', N'pass123', N'user01@gmail.com', N'Hà Nội', N'0987654321', 0, 1000000.5)
-GO
-INSERT [dbo].[Users] ([user__id], [username], [fullname], [password], [email], [address], [phone], [role], [balance]) VALUES (2, N'user02', N'Trần Thị B', N'pass456', N'user02@gmail.com', N'TP HCM', N'0912345678', 1, 500000.75)
-GO
 INSERT [dbo].[Users] ([user__id], [username], [fullname], [password], [email], [address], [phone], [role], [balance]) VALUES (3, N'user03', N'Phạm Văn C', N'pass789', N'user03@gmail.com', N'Đà Nẵng', N'0909876543', 0, 250000)
 GO
 INSERT [dbo].[Users] ([user__id], [username], [fullname], [password], [email], [address], [phone], [role], [balance]) VALUES (4, N'user04', N'Lê Thị D', N'pass101', N'user04@gmail.com', N'Cần Thơ', N'0922334455', 1, 300000.25)
@@ -167,28 +253,40 @@ INSERT [dbo].[Users] ([user__id], [username], [fullname], [password], [email], [
 GO
 INSERT [dbo].[Users] ([user__id], [username], [fullname], [password], [email], [address], [phone], [role], [balance]) VALUES (6, N'user06', N'Đoàn Hữu F', N'pass303', N'user06@gmail.com', N'Huế', N'0944556677', 1, 120000)
 GO
-INSERT [dbo].[Users] ([user__id], [username], [fullname], [password], [email], [address], [phone], [role], [balance]) VALUES (7, N'user07', N'Bùi Quang G', N'pass404', N'user07@gmail.com', N'Bình Dương', N'0955667788', 0, 450000.9)
-GO
-INSERT [dbo].[Users] ([user__id], [username], [fullname], [password], [email], [address], [phone], [role], [balance]) VALUES (8, N'user08', N'Vũ Hoàng H', N'pass505', N'user08@gmail.com', N'Vũng Tàu', N'0966778899', 1, 750000.3)
-GO
-INSERT [dbo].[Users] ([user__id], [username], [fullname], [password], [email], [address], [phone], [role], [balance]) VALUES (9, N'user09', N'Đặng Thanh I', N'pass606', N'user09@gmail.com', N'Nha Trang', N'0977889900', 0, 850000.15)
-GO
 INSERT [dbo].[Users] ([user__id], [username], [fullname], [password], [email], [address], [phone], [role], [balance]) VALUES (10, N'user10', N'Trịnh Hiền J', N'pass707', N'user10@gmail.com', N'Đồng Nai', N'0988990011', 1, 950000.6)
+GO
+INSERT [dbo].[Users] ([user__id], [username], [fullname], [password], [email], [address], [phone], [role], [balance]) VALUES (16, N'david_miller', N'David Miller', N'adminPass789', N'david.miller@example.com', N'303 Cedar Ln, North Haverbrook', N'555-8642', 1, 150)
+GO
+INSERT [dbo].[Users] ([user__id], [username], [fullname], [password], [email], [address], [phone], [role], [balance]) VALUES (18, N'charles_lee', N'Charles Lee', N'password1234', N'charles.lee@example.com', N'505 Redwood Dr, Shelbyville', N'555-6421', 1, 500)
 GO
 SET IDENTITY_INSERT [dbo].[Users] OFF
 GO
+SET ANSI_PADDING ON
+GO
+/****** Object:  Index [UC_User]    Script Date: 3/7/2025 10:27:43 PM ******/
+ALTER TABLE [dbo].[Users] ADD  CONSTRAINT [UC_User] UNIQUE NONCLUSTERED 
+(
+	[username] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+GO
 ALTER TABLE [dbo].[OrderDetails]  WITH CHECK ADD  CONSTRAINT [FK_OrderDetails_Orders] FOREIGN KEY([order__id])
 REFERENCES [dbo].[Orders] ([order__id])
+ON UPDATE CASCADE
+ON DELETE CASCADE
 GO
 ALTER TABLE [dbo].[OrderDetails] CHECK CONSTRAINT [FK_OrderDetails_Orders]
 GO
 ALTER TABLE [dbo].[OrderDetails]  WITH CHECK ADD  CONSTRAINT [FK_OrderDetails_Products] FOREIGN KEY([product__id])
 REFERENCES [dbo].[Products] ([product__id])
+ON UPDATE CASCADE
+ON DELETE CASCADE
 GO
 ALTER TABLE [dbo].[OrderDetails] CHECK CONSTRAINT [FK_OrderDetails_Products]
 GO
 ALTER TABLE [dbo].[Orders]  WITH CHECK ADD  CONSTRAINT [FK_Orders_Users] FOREIGN KEY([user__id])
 REFERENCES [dbo].[Users] ([user__id])
+ON UPDATE CASCADE
+ON DELETE CASCADE
 GO
 ALTER TABLE [dbo].[Orders] CHECK CONSTRAINT [FK_Orders_Users]
 GO
